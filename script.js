45856409eda6092d65e721c7fb29fed1f73395f4
inqyniqhts <afsharh9758@my.sduhsd.net>
2025-10-30 17:40:38 +0000

    almost forgot to commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // add javascript here
+let level, answer, score;
+
+//event listeners
+AnimationPlaybackEvent.addEventListener("click",play);
+guessBtn.addEventListener("click",makeGuess);
+
+function play(){
+    
+}
