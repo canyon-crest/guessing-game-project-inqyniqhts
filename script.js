58e97bf1c06689412717b927d78e7042fce00fc1
inqyniqhts <afsharh9758@my.sduhsd.net>
2025-10-30 17:49:26 +0000

    kbihv

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ let level, answer, score;
 const levelArr = document.getElementsByName("level");
 
 //event listeners
-AnimationPlaybackEvent.addEventListener("click",play);
+playBtn.addEventListener("click",play);
 guessBtn.addEventListener("click",makeGuess);
 
 function play(){
     playBtn.disabled= true;
     guessBtn.disabled=false;
     for(let i=0; i<levelArr.length;i++){
+        if(levelArr[i].checked){
+            level = levelArr[i].value;
+        }
         levelArr[i].disabled = true;
     }
-
-
+    msg.textcontent = "Guess a number from 1-"+level;
+    answer = Math.floor(Math.random()*level+1);
+    guess.placeholder = answer;
 }
