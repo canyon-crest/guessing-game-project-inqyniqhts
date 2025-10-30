e06ed9d13a3d03e7fd89dc9723fdaf5b5b76a1c1
inqyniqhts <afsharh9758@my.sduhsd.net>
2025-10-30 17:44:59 +0000

    bro

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // add javascript here
 let level, answer, score;
+const levelArr = document.getElementsByName("level");
 
 //event listeners
 AnimationPlaybackEvent.addEventListener("click",play);
 guessBtn.addEventListener("click",makeGuess);
 
 function play(){
-    AnimationPlaybackEvent.disabled= true;
-    
+    playBtn.disabled= true;
+    guessBtn.disabled=false;
+    for(let i=0; i<levelArr.length;i++){
+        levelArr[i].disabled = true;
+    }
+
+
 }
