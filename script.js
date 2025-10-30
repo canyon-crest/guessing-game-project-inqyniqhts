ca1196b0b8f5b78e46ce55804eafe30607fab17c
inqyniqhts <afsharh9758@my.sduhsd.net>
2025-10-30 18:13:32 +0000

    yayyyy

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ playBtn.addEventListener("click",play);
 guessBtn.addEventListener("click",makeGuess);
 
 function play(){
+    score = 0; // sets the score to 0 every new game
     playBtn.disabled= true;
     guessBtn.disabled=false;
     for(let i=0; i<levelArr.length;i++){
@@ -18,4 +19,32 @@ function play(){
     msg.textContent = "Guess a number from 1-"+level;
     answer = Math.floor((Math.random()*level)+1);
     guess.placeholder = answer;
+}
+
+function makeGuess(){
+    let userGuess = parseInt(guess.value);
+    if(isNaN(userGuess) || userGuess <1 || userGuess > level){
+        msg.textContent = "Enter a valid number from #1-" + level;
+        return;
+    }
+    score++; //add guess and one to score
+    if(userGuess > answer){
+        msg.textContent = "Too high!"
+    }
+    else if(userGuess < answer){
+        msg.textContent = "Too low!"
+
+    }
+    else{
+        msg.textContent = "You got it! It took you " + score +" tries to get the answer.";
+    }
+
+}
+
+function reset(){
+    guessBtn.disabled = true;
+    guess.disabled = true;
+    guess.value = "";
+    guess.placeholder="";
+    playBtn.disabled = false;
 }
